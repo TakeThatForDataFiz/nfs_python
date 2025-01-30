213501290275ad2eea4ed0b6a573641182457a8e
TakeThatForDataFiz <johnlunz8@gmail.com>
2025-01-29 19:38:26 -0500

    chapter 1 code tutorial run

---
+++ ch1/nfl_data_pull.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import nfl_data_py as nfl
+
+pbp_py = nfl.import_pbp_data([2024])
+filter_crit = 'play_type == "pass" & air_yards.notnull()'
+
+pbp_py_pass = (
+    pbp_py.query(filter_crit)
+    .groupby(["passer_id", "passer"])
+    .agg({"air_yards": ["count", "mean"]})
+)
+
+pbp_py_pass.columns = list(map("_".join, pbp_py_pass.columns.values))
+sort_crit = "air_yards_count > 100"
+
+print(
+    pbp_py_pass.query(sort_crit).sort_values(by="air_yards_mean", ascending=[False]).to_string()
+)
